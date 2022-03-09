b7d5f96ae4cbec3266ab6471fd1b0529759b6204
mosqu1t0 <mosquito@email.cn>
2022-03-09 23:13:11 +0800

    add trayer

--- sh/trayer.sh
+++ sh/trayer.sh
@@ -2,5 +2,5 @@
 killall trayer
 if [ $? != 0 ];
 then
-trayer --edge top --widthtype percent --width 12 --heighttype percent --height 40 --transparent true --alpha 30 --tint 0x002c323b &
+trayer --edge right --widthtype percent --width 13 --heighttype percent --height 40 --transparent true --alpha 30 --tint 0x002c323b &
 fi
