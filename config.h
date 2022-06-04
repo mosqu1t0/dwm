6c5045c1c4b90fd57ece3a47bfad13e509e845c6
mosqu1t0 <mosquito@email.cn>
2022-06-04 17:21:23 +0800

    change font size

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static const unsigned int gappx     = 8;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -10,7 +10,7 @@ static const Bool viewontag         = True;     /* Switch view on tag switch */
 
 static const char *fonts[]          = { "JetBrains Mono:style=Bold:size=12",
 					"WenQuanYi Micro Hei Mono:size=12:type=Regular:antialias=true:autohint=true",
-					"Symbols Nerd Font:pixelsize=20:type=1000-em:antialias=true:autohint=true"};
+					"Symbols Nerd Font:pixelsize=22:type=1000-em:antialias=true:autohint=true"};
 static const char dmenufont[]       = "JetBrains Mono:style=Bold:size=12";
 static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#282828";
